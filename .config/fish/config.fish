82eb9219f4af39cf465eb38a349606775596bf21
danielulisses <danielulisses@outlook.com>
2024-05-16 11:45:19 -0300

    added nodejs path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+set -U fish_user_paths ~/.tmuxifier/bin $fish_user_paths
 set fish_greeting
 starship init fish | source
+
